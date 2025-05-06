9090982cd86c766407f11202f9dc073fe4862173
ibpds <pordeus.school>
2025-05-06 11:48:23 -0300

    Assignment

--- GUICalculator.java
+++ GUICalculator.java
@@ -8,5 +8,61 @@ public class GUICalculator {
         final JTextField aText = new JTextField("");
         final JTextField bText = new JTextField("");
         final JLabel equalLabel = new JLabel("="); 
+        final JLabel resultLabel = new JLabel("0.0");
+
+        JButton addButton = new JButton("+");
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                int a = Integer.parseInt(aText.getText());
+                int b = Integer.parseInt(bText.getText());
+                int result = a + b;
+                resultLabel.setText(Integer.toString(result));
+            }
+        });
+
+        JButton subButton = new JButton("-");
+        subButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                int a = Integer.parseInt(aText.getText());
+                int b = Integer.parseInt(bText.getText());
+                int result = a - b;
+                resultLabel.setText(Integer.toString(result));
+            }
+        });
+
+        JButton multButton = new JButton("X");
+        multButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                int a = Integer.parseInt(aText.getText());
+                int b = Integer.parseInt(bText.getText());
+                int result = a * b;
+                resultLabel.setText(Integer.toString(result));
+            }
+        });
+
+        JButton divButton = new JButton(":");
+        divButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                int a = Integer.parseInt(aText.getText());
+                int b = Integer.parseInt(bText.getText());
+                int result = a / b;
+                resultLabel.setText(Integer.toString(result));
+            }
+        });
+        frame.setLayout(new GridLayout(2,4));
+        frame.add(aText);
+        frame.add(bText);
+        frame.add(equalLabel);
+        frame.add(resultLabel);
+        frame.add(addButton);
+        frame.add(subButton);
+        frame.add(multButton);
+        frame.add(divButton);
+        frame.setSize(300, 100);
+        frame.setVisible(true);
     }
 }
