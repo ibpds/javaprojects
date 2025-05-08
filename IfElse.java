b1e7987e3df9de7692a94867d8143209f1926ef5
iza <pordeus.school@gmail.com>
2025-05-08 09:45:37 -0300

    ifelse

---
+++ IfElse.java
@@ -0,0 +1,13 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int score = 5;
+        
+        if(score == 5){
+            System.out.println("Excellent");
+        }else if(score == 4){
+            System.out.println("Good");
+        }else{
+            System.out.println("Need to catch up");
+        }
+    }
+}
